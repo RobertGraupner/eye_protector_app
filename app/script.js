da43fff1951cb37b986f6ce814b2e5ccc04a4ffe
Robert Graupner <robert.graupnerr@gmail.com>
2022-05-20 17:36:40 +0200

    add functionality

--- app/script.js
+++ app/script.js
@@ -3,23 +3,87 @@ import { render } from 'react-dom';
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
-        <button className="btn btn-close">X</button>
-      </div>
-    )
-  }
-};
+	state = {
+		status: 'off',
+		time: 0,
+		timer: null,
+	};
+
+	formatTime = time => {
+		let minutes = Math.floor(time / 60);
+		let seconds = time - (minutes * 60);
+
+		minutes < 10 ? (minutes = '0' + minutes) : minutes;
+		seconds < 10 ? (seconds = '0' + seconds) : seconds;
+
+		return minutes + ':' + seconds;
+	};
+// funkcja zmniejsza czas o 1 i sprawdza status
+	step = () => {
+		this.setState({time: this.state.time - 1});
+
+		if (this.state.time == 0) {
+			this.playBell();
+			if (this.state.status == 'work') {
+				this.setState({
+					status: 'rest',
+					time: 20,
+				})
+			} else if (this.state.status == 'rest') {
+				this.setState({
+					status: 'work',
+					time: 1200,
+				})
+			};
+		}
+	}
+// ustawiamy wartość po kliknięciu przycisku Start
+	startTimer = () => {
+		this.setState({
+			time: 1200,
+			status: 'work',
+			timer: setInterval(this.step, 1000),
+		});
+	};
+// ustawiamy wartość po kliknięciu przycisku Stop
+	stopTimer = () => {
+		this.setState({
+			time: 0,
+			status: "off",
+			timer: clearInterval(this.state.timer),
+		});
+	};
+
+	playBell = () => {
+		const bell = new Audio('./sounds/bell.wav');
+		bell.play();
+	};
+
+	closeApp = () => {
+		window.close();
+	};
+
+	render() {
+		const { status} = this.state;
+
+		return (
+			<div>
+				<h1>Protect your eyes</h1>
+				{status === 'off' && (
+					<div>
+						<p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+						<p>This app will help you track your time and inform you when it'stime to rest.</p>
+					</div>
+				)}
+				{status === 'work' && <img src='./images/work.png' />}
+				{status === 'rest' && <img src='./images/rest.png' />}
+				{status !== 'off' && (<div className='timer'>{this.formatTime(this.state.time)}</div>)}
+				{status === 'off' && (<button className='btn' onClick={() => this.startTimer()}>Start</button>)}
+				{status !== 'off' && (<button className='btn' onClick={() => this.stopTimer()}>Stop</button>)}
+				<button className='btn btn-close' onClick={() => this.closeApp()}>X</button>
+			</div>
+		);
+	}
+}
 
 render(<App />, document.querySelector('#app'));
--- main.js
+++ main.js
@@ -7,18 +7,19 @@ function main() {
 
   // create new window
   let mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
-    },
-    width: 800,
-    height: 600,
-  })
+		webPreferences: {
+			nodeIntegration: true,
+		},
+		width: 520,
+    height: 650,
+    frame: false,
+	});
 
   // load app/index.html as the window content
   mainWindow.loadFile(path.join('app', 'index.html'));
 
   // open devtools
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', main);
